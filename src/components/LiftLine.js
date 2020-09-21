adb301af7fd42c925fdcf15788e1193c75e4e903
Leonard Kulms <leonard.kulms@icloud.com>
2020-09-21 23:17:07 +0200

    add rudimentary addlift feature

--- src/components/LiftLine.js
+++ src/components/LiftLine.js
@@ -1,35 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Lift from './Lift'
+import NewLift from './NewLift'
 
 function LiftLine(props) {
-  const exercises = ['Bench', 'Deadlift','Squat']
+  const [lifts, setLifts] = useState([])
 
-  const random_item = (items) => items[Math.floor(Math.random()*items.length)]
+  const addLift = (newLift)  => {
+    setLifts([...lifts, newLift])
+  }
 
-  let fakeData = new Array(100).fill([{
-    exercise: random_item(exercises),
-    sets: 1 + Math.floor(Math.random()* 5),
-    reps: 1 + Math.floor(Math.random()* 10),
-    weight: Math.floor(Math.random()*120),
-    likedByUser: Math.floor(Math.random()*2),
-    fireCount: Math.floor(Math.random()*30)
-  }]).flat();
-
-  const liftComponents = fakeData.map(
-    lift => <Lift 
-      theme= { props.theme }
-      sets={ lift.sets }
-      reps={ lift.reps }
-      weight={ lift.weight }
-      exercise={ lift.exercise}
-      fireCount={lift.fireCount}
-      likedByUser={ lift.likedByUser}/>)
+  // use only on the first render
+  // useEffect( () => {
+  //   let exercises = ['Bench', 'Deadlift','Squat']
+  //   let random_item = (items) => items[Math.floor(Math.random()*items.length)]
   
+  //   let fakeData = new Array(20).fill([{
+  //     exercise: random_item(exercises),
+  //     sets: 1 + Math.floor(Math.random()* 5),
+  //     reps: 1 + Math.floor(Math.random()* 10),
+  //     weight: Math.floor(Math.random()*120),
+  //     likedByUser: Math.floor(Math.random()*2),
+  //     fireCount: Math.floor(Math.random()*30)
+  //   }]).flat();
+
+  //   setLifts(lifts => fakeData)
+  // }, [])
+
   return(
     <ul className="app--liftline">
-      { liftComponents }
+      <NewLift theme = { props.theme } addLift = { addLift }/>
+      { lifts.map( lift => <Lift 
+          theme= { props.theme }
+          sets={ lift.sets }
+          reps={ lift.reps }
+          weight={ lift.weight }
+          exercise={ lift.exercise }
+          fireCount={lift.fireCount }
+          likedByUser={ lift.likedByUser }/>) }
     </ul>
-
   )
 }
 
