c06582a65f180cd4816fc88d339dc2b18cf9db2b
Leonard Kulms <leonard.kulms@icloud.com>
2020-09-29 18:16:50 +0200

    fix lift errors

--- src/components/Lift.js
+++ src/components/Lift.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Avatar from './Avatar'
 
 function Lift(props) {
   const [fireCount, setFireCount] = useState(props.fireCount)
-  const [likedByUser, setLikedByUser] = useState(props.fireCount)
+  const [likedByUser, setLikedByUser] = useState(props.likedByUser)
+
+  useEffect( () => {
+    setFireCount(props.fireCount)
+    setLikedByUser(props.likedByUser)
+  }, [props.fireCount, props.likedByUser])
 
   const handleClick = () => {
     (likedByUser) ? setFireCount(fireCount - 1) : setFireCount(fireCount + 1)
@@ -14,17 +19,14 @@ function Lift(props) {
   return(
     <li className= {'app--lift ' + ((props.theme)? 'dark' : 'light')}>
       <Avatar />
-      <p>{ props.exercise }</p>
-      
       <div className='app--lift-stats'>
+        <p>{ props.exercise },</p>
         <p className='app--lift-item'>{ props.sets } X </p>
         <p className='app--lift-item'>{ props.reps } </p>
         <p className='app--lift-item'>mit { props.weight }</p>
       </div>
-
+      <p><span aria-label='fire' role='img'>🔥</span>{ fireCount }</p>
       <div className='app--lift-reaction'>
-        <p><span aria-label='fire' role='img'>🔥</span>{ fireCount }</p>
-
         <button onClick={ () => handleClick() } className= {'app--lift-reaction-button ' + ((likedByUser)? 'is-active':'')}>
           <span className='app--lift-reaction-icon' aria-label='fire' role='img'>🔥</span>
         </button>
