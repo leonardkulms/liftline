45df36fb8f453661fb6e4cea5cf4d2cc460ed969
Leonard Kulms <leonard.kulms@icloud.com>
2020-09-29 18:17:20 +0200

    improve generating random fake lifts

--- src/components/LiftLine.js
+++ src/components/LiftLine.js
@@ -6,29 +6,30 @@ function LiftLine(props) {
   const [lifts, setLifts] = useState([])
 
   const addLift = (newLift)  => {
-    setLifts([...lifts, newLift])
+    setLifts([ newLift, ...lifts ])
   }
 
   // use only on the first render
-  // useEffect( () => {
-  //   let exercises = ['Bench', 'Deadlift','Squat']
-  //   let random_item = (items) => items[Math.floor(Math.random()*items.length)]
+  useEffect( () => {
+    let exercises = ['Bench', 'Deadlift','Squat']
+    let random_item = (items) => items[Math.floor(Math.random()*items.length)]
   
-  //   let fakeData = new Array(20).fill([{
-  //     exercise: random_item(exercises),
-  //     sets: 1 + Math.floor(Math.random()* 5),
-  //     reps: 1 + Math.floor(Math.random()* 10),
-  //     weight: Math.floor(Math.random()*120),
-  //     likedByUser: Math.floor(Math.random()*2),
-  //     fireCount: Math.floor(Math.random()*30)
-  //   }]).flat();
+    let fakeData = new Array(20).fill([{
+      exercise: random_item(exercises),
+      sets: 1 + Math.floor(Math.random()* 5),
+      reps: 1 + Math.floor(Math.random()* 10),
+      weight: Math.floor(Math.random()*120),
+      likedByUser: Math.floor(Math.random()*2),
+      fireCount: Math.floor(Math.random()*30)
+    }]).flat();
 
-  //   setLifts(lifts => fakeData)
-  // }, [])
+    setLifts(lifts => fakeData)
+  }, [])
 
   return(
     <ul className="app--liftline">
       <NewLift theme = { props.theme } addLift = { addLift }/>
+  
       { lifts.map( lift => <Lift 
           theme= { props.theme }
           sets={ lift.sets }
@@ -37,6 +38,7 @@ function LiftLine(props) {
           exercise={ lift.exercise }
           fireCount={lift.fireCount }
           likedByUser={ lift.likedByUser }/>) }
+
     </ul>
   )
 }
