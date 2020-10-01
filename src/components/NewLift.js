952d222dc7b7e3584203449b6cf6102d8143d4c2
Leonard Kulms <leonard.kulms@icloud.com>
2020-10-01 17:03:05 +0200

    :art: refactor newlift forms

--- src/components/NewLift.js
+++ src/components/NewLift.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { useForm } from 'react-hook-form'
 import Modal from './Modal'
+import NewLiftForm from './NewLiftForm'
 
 function NewLift(props) {
   const [isModalActive, setModalIsActive] = useState(false)
@@ -8,50 +8,17 @@ function NewLift(props) {
 		setModalIsActive(!isModalActive)
 	}
 
-  const { register, handleSubmit, watch, errors } = useForm()
-  const onSubmit = data => {
-    props.addLift({
-      sets: data.sets,
-      reps: data.reps,
-      weight: data.weight,
-      exercise: data.exercise,
-      fireCount: 0,
-      likedByUser: false})
-    toggleModal()
-  }
-
   return (
-    <div className="app--newlift-container">
+    <div className={ 'app--newlift-container ' + ((props.theme)? 'dark ' : 'light ')} >
       {isModalActive
-        ?<Modal heading="Add a new Lift!" onRequestClose={toggleModal} >
-          <form 
-            className={'app--newlift ' + ((props.theme)? 'dark' : 'light')}
-            onSubmit={handleSubmit(onSubmit)}>
-            <div className="app--newlift-form-group">
-              <label htmlFor="exercise">Exercise:</label>
-              <select name="exercise" ref={register({ required: true })}>
-                <option>Deadlift</option>
-                <option>Bench</option>
-                <option>Squat</option>
-              </select>
-            </div>
-            <div className="app--newlift-form-group">
-              <label htmlFor="weight">Weight:</label>
-              <input name="weight" type="number" min="20" max="300" ref={register({ required: true })} />
-            </div>
-            <div className="app--newlift-form-group">
-              <label htmlFor="reps">Reps:</label>
-              <input  type="number" min="1" max="20" name="reps" ref={register({ required: true })} />
-            </div>
-            <div className="app--newlift-form-group">
-            <label htmlFor="sets">Reps:</label>
-            <input  type="number" min="1" max="20" name="sets" ref={register({ required: true })} />
-            {/* {errors.exampleRequired && <span>This field is required</span>} */}
-            </div>
-            <button class="app--newlift-button" type="submit">Add Lift</button>
-          </form>
+        ?<Modal heading="Add a new Lift!" onRequestClose={ toggleModal } theme={ props.theme } >
+          <NewLiftForm toggleModal={ toggleModal } addLift={ props.addLift } />
         </Modal>
-      :<button onClick={toggleModal} type="button" className="app--newlift-button">➕
+      :<button 
+        onClick={toggleModal} 
+        type="button" 
+        className="app--newlift-button"
+        >➕
       </button>}
     </div>
   );
